80f997b17d45b65b2c91df81e19db26423c96237
Bernardthe3rd <bammeijer@outlook.com>
2024-01-31 10:09:33 +0100

    start projet part one

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Begin hier met met maken van de applicatie!</h1>
+      <>
+      <h1>Begin hier met met maken van de applicatie!</h1>
+      <p>first lines of code for github</p>
+      </>
   )
 }
 
