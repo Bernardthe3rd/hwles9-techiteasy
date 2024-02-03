06ec99e2e6117fe77918cd84442400180289db77
Bernardthe3rd <bammeijer@outlook.com>
2024-02-03 15:55:58 +0100

    started part 2

---
+++ src/constants/inventory-practice.js
@@ -0,0 +1,9 @@
+import {inventory} from "./inventory.js";
+
+// Opdracht 1a: Gebruik een array-methode om een array te maken met daarin alle tv-type namen. Log de uitkomst in de console.
+// Opdracht 1b: Gebruik een array-methode om alle informatie te verzamelen van de tv's die volledig uitverkocht zijn. Log de uitkomst in de console.
+// Opdracht 1c: Gebruik een array-methode om de informatie van het tv-type 'NH3216SMART' op te halen.
+// Opdracht 1d: Gebruik een array-methode om een lijst te maken van de merk- en tv-namen waarbij aangegeven wordt of deze geschikt zijn voor sport-kijkers (een verversingssnelheid van 100Hz of hoger).
+// Doe dit in het format { name: 'Toshiba HD TV', suitable: false }. Log de uitkomst in de console.
+// Opdracht 1e (uitdaging): Gebruik array-methoden om alle informatie te verzamelen van de tv's die beschikbaar zijn in schermgroottes van 65 inch en groter.
+// Opdracht 1f (uitdaging): Gebruik array-methoden om alle informatie te verzamelen van de tv's die over ambilight beschikken. Log de uitkomst in de console.
